ee95c3cc47df20834c5b2605a775f2841c524809
Prakash A <prakashaathi321@gmail.com>
2023-04-27 13:39:23 +0530

    changed db

--- src/localstorage/Db.js
+++ src/localstorage/Db.js
@@ -1,163 +1,163 @@
 export const Db = [
     {
-        name: "Adai.jpg",
+        name: "Adai.jpeg",
         nutrients:[29.5,8.04,6.1,6.4,203,72,462]
     },
     {
-        name: "Bread Jam.jpg",
+        name: "Bread Jam.jpeg",
         nutrients:[57,5.3,2.7,2,263,290,102]
     },
     {
-        name: "Idiyappam.jpg",
+        name: "Idiyappam.jpeg",
         nutrients:[44,3,2,5.8,271,2312,93]
     },
 	{
-        name: "Kadala Curry.jpg",
+        name: "Kadala Curry.jpeg",
         nutrients:[41,15,13.6,2.4,226,39.2,405.6]
     },
 	{
-        name: "Naan.jpg",
+        name: "Naan.jpeg",
         nutrients:[47,9,7.1,5.4,270,423,119]
     },
 	{
-        name: "Oats.jpg",
+        name: "Oats.jpeg",
         nutrients:[64,11.7,10.6,5.2,326,5.1,302]
     },
 	{
-        name: "Parotta",
+        name: "Parotta.jpeg",
         nutrients:[52,8,0.4,15,493,685,215]
     },
 	{
-        name: "Kichadi.jpg",
+        name: "Kichadi.jpeg",
         nutrients:[55,15,12,4.6,310,163,564]
     },
 	{
-        name: "Puttu.jpg",
+        name: "Puttu.jpeg",
         nutrients:[30.2,3,1.8,2,143,42.5,114.2]
     },
 	{
-        name: "Upma.jpg",
+        name: "Upma.jpeg",
         nutrients:[19,3.2,0.3,3.7,128,173,187]
     },
 	{
-        name: "Idly.jpg",
+        name: "Idly.jpeg",
         nutrients:[36,5,1,1,174,225,123]
     },
 	{
-        name: "Dosa.jpg",
+        name: "Dosa.jpeg",
         nutrients:[29,3.9,1,3.5,168,94,80]
     },
 	{
-        name: "Sweet Pongal.jpg",
+        name: "Sweet Pongal.jpeg",
         nutrients:[48.7,3.8,1.4,6.9,272,2.1,107.2]
     },
 	{
-        name: "Venn Pongal.jpg",
+        name: "Venn Pongal.jpeg",
         nutrients:[27,3.6,5,4.1,250,3.4,194]
     },
 	{
-        name: "Appam.jpg",
+        name: "Appam.jpeg",
         nutrients:[48,4,0.9,6.7,275,204,98]
     },
 	{
-        name: "Poori.jpg",
+        name: "Poori.jpeg",
         nutrients:[33,6.3,1.3,25.4,413,244,179]
     },
 	{
-        name: "Toast.jpg",
+        name: "Toast.jpeg",
         nutrients:[60,10,4.7,4.4,320,590,145]
     },
 	{
-        name: "Uthappam.jpg",
+        name: "Uthappam.jpeg",
         nutrients:[27.4,3.3,3.4,5.8,176,18.9,250.3]
     },
 	{
-        name: "Appalam.jpg",
+        name: "Appalam.jpeg",
         nutrients:[40.8,15.6,8.2,13.2,348,480,636]
     },
 	
 	{
-        name: "Black Sundal.jpg",
+        name: "Black Sundal.jpeg",
         nutrients:[22,9,8,0,134,5,434]
     },
 	{
-        name: "Egg Curry.jpg",
+        name: "Egg Curry.jpeg",
         nutrients:[7.54,5.95,1.9,7.11,114,253,221]
     },
 	{
-        name: "Chicken 65.jpg",
+        name: "Chicken 65.jpeg",
         nutrients:[7.9,3.3,0.2,7.7,119,423]
     },
 	{
-        name: "Fish Curry.jpg",
+        name: "Fish Curry.jpeg",
         nutrients:[2.9,12.6,0.5,4.59,100,169,289]
     },
 	{
-        name: "Fish Fry.jpg",
+        name: "Fish Fry.jpeg",
         nutrients:[7.81,18.26,0.5,11.36,210,279,207]
     },
 	{
-        name: "Gobi Manchurian.jpg",
+        name: "Gobi Manchurian.jpeg",
         nutrients:[21.3,2.9,1.8,8.95,175,82,169]
     },
 	{
-        name: "Kozhukatai.jpg",
+        name: "Kozhukatai.jpeg",
         nutrients:[91,6,4,3.7,450,207,52]
     },
 	{
-        name: "Medhu Vadai.jpg",
+        name: "Medhu Vadai.jpeg",
         nutrients:[42.17,13.75,15.6,10.02,309,326,999]
     },
 	{
-        name: "Mint Thovayal.jpg",
+        name: "Mint Thovayal.jpeg",
         nutrients:[7,0,1.8,3.5,52.5,1715,0]
     },
 	{
-        name: "Mushroom Masala.jpg",
+        name: "Mushroom Masala.jpeg",
         nutrients:[64.6,10,4.8,4,328,6580,262]
     },
 	{
-        name: "Kuruma.jpg",
+        name: "Kuruma.jpeg",
         nutrients:[11.04,2.58,2.47,5.98,109.4,13.82,213.4]
     },
 	{
-        name: "Panner Butter Masala.jpg",
+        name: "Panner Butter Masala.jpeg",
         nutrients:[8.83,7.68,1.1,16.6,211,306,212]
     },
 	{
-        name: "Paniyaram.jpg",
+        name: "Paniyaram.jpeg",
         nutrients:[39.25,5.73,3.2,5.71,232,251,186]
     },
 	{
-        name: "Parupu Payasam.jpg",
+        name: "Parupu Payasam.jpeg",
         nutrients:[33,6,1,25,413,244,179]
     },
 	{
-        name: "Potato Fry.jpg",
+        name: "Potato Fry.jpeg",
         nutrients:[48,4,4.4,17,365,246,677]
     },
 	{
-        name: "Sambar Vada.jpg",
+        name: "Sambar Vada.jpeg",
         nutrients:[22,8.8,4,16.8,270,432,254]
     },
 	{
-        name: "Samosa.jpg",
+        name: "Samosa.jpeg",
         nutrients:[32.1,4.67,1.7,17.86,308,816,185]
     },
 	{
-        name: "Curd Vada.jpg",
+        name: "Curd Vada.jpeg",
         nutrients:[23.89,8.26,4.3,12.7,242,402,373]
     },
 	{
-        name: "Pakoda.jpg",
+        name: "Pakoda.jpeg",
         nutrients:[15.47,4.56,4.3,4.59,118,1103,246]
     },
 	{
-        name: "Mutton Curry.jpg",
+        name: "Mutton Curry.jpeg",
         nutrients:[5.6,4.6,0.6,5.5,68.91,712,92]
     },
 	{
-        name: "Chicken Curry.jpg",
+        name: "Chicken Curry.jpeg",
         nutrients:[62.09,11.27,0,9.73,381,4152,404]
     }
 ]
